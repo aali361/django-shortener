cc548f8022a2e04428902e707dc08c156eedbb33
ghorbani <aali361@gmail.com>
2020-10-08 15:25:08 +0330

    add user app

--- shortener/shortener/urls.py
+++ shortener/shortener/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/shortener/', include('app.urls')),
+    path('v1/user/', include('user.urls')),
 ]
